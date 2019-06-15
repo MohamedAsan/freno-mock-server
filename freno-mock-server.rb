2158e29bd9a08e75391a900a028f0954db970c79
MohamedAsan <mohamed.asan@freshworks.com>
2019-06-15 21:33:04 +0530

    Use faker to generate response value

--- freno-mock-server.rb
+++ freno-mock-server.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'json'
+require 'faker'
 
 get '/' do
   status_code = [200, 417, 429, 500].sample
-  response_value = 23
+  response_value = Faker::Number.within(0..2000).to_i/1000.0
   puts "Status Code: #{status_code} | Response Value: #{response_value}"
 
   status status_code
