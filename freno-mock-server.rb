cd2978110030a2c6144b0ddb3ccf0cc3d3cec6ad
MohamedAsan <mohamed.asan@freshworks.com>
2019-06-18 23:09:50 +0530

    Revert "Respond with only 200"
    This reverts commit e614afa9bde2e8462e27603c0dd24e3b96666bbd.

--- freno-mock-server.rb
+++ freno-mock-server.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'faker'
 
 get '/' do
-  status_code = 200
+  status_code = 429
   response_value = Faker::Number.within(60000..100000).to_i/1000.0
   puts "Status Code: #{status_code} | Response Value: #{response_value}"
 
