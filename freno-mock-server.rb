19f55fb5de145cbabe128ae856078ab611e0e9c1
Mohamed Asan N <mohamed.asan@freshworks.com>
2020-10-01 22:33:36 +0530

    Change Response Body (#5)

--- freno-mock-server.rb
+++ freno-mock-server.rb
@@ -4,10 +4,21 @@ require 'faker'
 
 get '/' do
   status_code = [200, 417, 429, 500, 503].sample
-  response_value = Faker::Number.within(0..100000).to_i/1000.0
-  puts "Status Code: #{status_code} | Response Value: #{response_value}"
 
+  body = {
+    'Status' => status_code
+  }
+  body['Message'] = case status_code
+                    when 200 then 'No Replication Lag'
+                    when 417 then 'Application Throttled'
+                    when 429 then 'Replication Lag'
+                    when 500 then 'Internal Server Error'
+                    when 503 then 'Service Unavailable'
+                    end
+  body['Value'] = Faker::Number.within(0..100000).to_i/1000.0 if status_code.eql?(429)
+
+  puts body
   status status_code
   content_type :json
-  {'Status' => status_code, 'Value' => response_value}.to_json
+  body.to_json
 end
